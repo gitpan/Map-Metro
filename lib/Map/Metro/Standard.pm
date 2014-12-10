d7aa5d1adeb18e7cff8491a54e42855d3cf92445
Erik Carlsson <info@code301.com>
2014-12-10 12:13:02 +0000

    Import of CSSON/Map-Metro-0.1495.tar.gz from CPAN.
    gitpan-cpan-distribution: Map-Metro
    gitpan-cpan-version:      0.1495
    gitpan-cpan-path:         CSSON/Map-Metro-0.1495.tar.gz
    gitpan-cpan-author:       CSSON
    gitpan-cpan-maturity:     released

--- lib/Map/Metro/Standard.pm
+++ lib/Map/Metro/Standard.pm
@@ -2,7 +2,7 @@ use 5.20.0;
 use warnings;
 
 package Map::Metro::Standard {
-$Map::Metro::Standard::VERSION = '0.1494';
+$Map::Metro::Standard::VERSION = '0.1495';
 use Syntax::Collector q/
         use feature 0 ':5.20';
         use warnings 0;
